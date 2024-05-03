df8fe598a2bc4d8ef2a0c9e0361396e8d7742c75
Echo <91438643+echo-111@users.noreply.github.com>
2024-05-03 17:51:04 +1000

    add layout

---
+++ app/layout.tsx
@@ -0,0 +1,59 @@
+import { Toaster } from "sonner";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { EdgeStoreProvider } from "@/lib/edgestore";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Notion-A",
+  description: "The connected workspace where better, faster work happens.",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo-light.png",
+        href: "/logo-light.png",
+      },
+      {
+        media: "(prefers-color-scheme: dark )",
+        url: "/logo-dark.png",
+        href: "/logo-dark.png",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <EdgeStoreProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="notion-a-theme"
+          >
+            <Toaster position="bottom-center"/>
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
+          </EdgeStoreProvider>
+        </ConvexClientProvider>
+      </body>
+    </html>
+  );
+}
